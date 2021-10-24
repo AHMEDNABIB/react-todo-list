285656a07927bdb069ff86389124fe1a00e1d061
AHMEDNABIB <nabibpallab@gmail.com>
2021-10-24 21:11:07 +0600

    first commit of todo list

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello from todo input</p>
+            </div>
+        )
+    }
+}
+
+export default TodoInput
---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); 
+
+ class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-4 bg-danger">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
